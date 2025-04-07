4ab80a9c6475a8ed679bc8516580f1003df4473b
Boris <unmasked.unmasked@gmail.com>
2025-04-07 10:34:11 +0300

    changed UrlRepository class, added custom search method to make it clearer and removed unnecessary parts

--- src/Repository/UrlRepository.php
+++ src/Repository/UrlRepository.php
@@ -6,9 +6,6 @@ use App\Entity\Url;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Url>
- */
 class UrlRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
@@ -16,28 +13,9 @@ class UrlRepository extends ServiceEntityRepository
         parent::__construct($registry, Url::class);
     }
 
-    //    /**
-    //     * @return Url[] Returns an array of Url objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function findByShortCode(string $shortCode): ?Url
+    {
+        return $this->findOneBy(['shortCode' => $shortCode]);
+    }
 
-    //    public function findOneBySomeField($value): ?Url
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
